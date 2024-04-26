88d247f29ee2ba7b6b2d50a06c1da8b8e63ac879
Екатерина Жакова <zhakova01@mail.ru>
2024-04-26 12:50:50 +0000

    Update routes.rb

--- ticket_server/config/routes.rb
+++ ticket_server/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-    get 'tickets/free'
+  get 'tickets/free'
 
-    put 'tickets/status', to: 'tickets#update'
+  put 'tickets/status', to: 'tickets#update'
 
-    put 'tickets/block'
+  put 'tickets/block'
 
-    get 'tickets', to: 'tickets#show'
-
+  get 'tickets', to: 'tickets#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
